57ccf2329e6ce6698b61027a345a02a569313d61
Yassine Benlahbib <yassinebnlhbb@gmail.com>
2024-05-26 20:15:34 +0100

    first commit

---
+++ src/main/java/com/ensa/deploytest/app/controller/AppController.java
@@ -0,0 +1,20 @@
+package com.ensa.deploytest.app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AppController {
+
+    @GetMapping("api/hello")
+    public ResponseEntity<Map<String, String>> hello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello World!");
+        return ResponseEntity.ok(response);
+
+    }
+}
